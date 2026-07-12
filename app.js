7ccdd263061be7ccecb3d271f756289f1b573da2
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-07-12 18:36:06 +0500

    working

---
+++ app.js
@@ -0,0 +1,11 @@
+// script.js
+const carouselEl = document.querySelector("#home2Carousel");
+
+if (carouselEl) {
+  const carousel = new bootstrap.Carousel(carouselEl, {
+    interval: 3500,
+    ride: "carousel",
+    pause: "hover",
+    wrap: true
+  });
+}
