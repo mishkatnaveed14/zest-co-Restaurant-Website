2a82b072fffbdbb5495f8e8b43478262aced090c
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-08-08 20:57:05 +0500

    working on signin authentication

--- firebase.config.js
+++ firebase.config.js
@@ -1,13 +1,24 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBm9kCZhDtXJOK148a-8acVIoLBux4w2Pc",
-    authDomain: "zestandco-7b360.firebaseapp.com",
-    projectId: "zestandco-7b360",
-    storageBucket: "zestandco-7b360.firebasestorage.app",
-    messagingSenderId: "345032386958",
-    appId: "1:345032386958:web:ea3bba37f8d0a8d46a1cdf"
-  };
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBm9kCZhDtXJOK148a-8acVIoLBux4w2Pc",
+  authDomain: "zestandco-7b360.firebaseapp.com",
+  projectId: "zestandco-7b360",
+  storageBucket: "zestandco-7b360.firebasestorage.app",
+  messagingSenderId: "345032386958",
+  appId: "1:345032386958:web:ea3bba37f8d0a8d46a1cdf",
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+//  export
+export { 
+    auth,
+     createUserWithEmailAndPassword
+ };
